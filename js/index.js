cffb5850563c8a1457ebddf8359702c9ac4599ba
ignacio alcaniz <ignaalcaniz@gmail.com>
2024-05-07 17:54:06 -0300

    segunda estrucutura del proyecto

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
+// let nombre=prompt("ingrese su nombre");
+// let edad=prompt("ingrese su edad");
+// let resultado="hola"+ " " +nombre+ " "+ "de"+ " "+ edad+ " " + "anios";
+// alert(resultado);
+// edad=parseInt(edad);
+// let calculoDeEdad=2024-edad;
+// alert("usted nacio en el anio"+ " "+calculoDeEdad);
+// if (edad>=18) {
+//     alert("usted es mayor de edad");
+ 
+// }   else{
+//     alert("usted es menor de edad")
+// };
 
 
